35c646198a9ecf39964357391eab52d0ada8b023
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2020-01-05 16:18:08 +0100

    Change project structure due to an error from chalice in the relative project directories

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from chalice import Chalice
 
-app = Chalice(app_name='app')
+app = Chalice(app_name='cosa')
 
 
 @app.route('/')
@@ -13,15 +13,15 @@ def index():
 #
 # Here are a few more examples:
 #
-# @app.route('/hello/{name}')
+# @application.route('/hello/{name}')
 # def hello_name(name):
 #    # '/hello/james' -> {"hello": "james"}
 #    return {'hello': name}
 #
-# @app.route('/users', methods=['POST'])
+# @application.route('/users', methods=['POST'])
 # def create_user():
 #     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
+#     user_as_json = application.current_request.json_body
 #     # We'll echo the json body back to the user in a 'user' key.
 #     return {'user': user_as_json}
 #
